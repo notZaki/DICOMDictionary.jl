c48d668aa7a9676e7ece6b5c3cc6ccc4aed5fa92
Zaki A <zaki@live.ca>
2020-08-01 09:14:23 -0400

    Modify push script; trying to avoid force pushes

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,19 +6,17 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_file() {
+send_to_github() {
   git checkout --orphan dcm_dict
   git rm -rf .
+  git remote add origin-pages https://${GH_TOKEN}@github.com/notZaki/DICOM_Dictionary.jl > /dev/null 2>&1
+  git pull origin-pages dcm_dict
   cp test/dcm_dict.jl ./dcm_dict.jl
   git add dcm_dict.jl
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-push_to_github() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/notZaki/DICOM_Dictionary.jl > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages dcm_dict
 }
 
+
 setup_git
-commit_file
-push_to_github
+send_to_github
